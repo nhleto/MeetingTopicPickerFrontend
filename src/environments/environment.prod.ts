1ef4fcd2c8d0b5f6ab0edef5862db58480909608
Henry Leto <hnr.leto11@gmail.com>
2021-10-31 18:35:17 -0400

    firebaseconfig

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  // Yes, this is fine to be in VC
+  firebaseConfig: {
+    apiKey: 'AIzaSyD4qaUIO3glpmO1nTL0NLtz9FH2zQtAv5E',
+    authDomain: 'meeting-topic-picker.firebaseapp.com',
+    projectId: 'meeting-topic-picker',
+    storageBucket: 'meeting-topic-picker.appspot.com',
+    messagingSenderId: '543883783924',
+    appId: '1:543883783924:web:819e4782fe279e5db2b8a1',
+    measurementId: 'G-3FBLS0ZZLS'
+  }
 };
